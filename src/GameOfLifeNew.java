e5f473b11060a24779419c4eee840f8ccfd819b0
m-m-e <megmyfedwards@gmail.com>
2019-07-30 15:51:51 +0200

    Fix new solution

--- src/GameOfLifeNew.java
+++ src/GameOfLifeNew.java
@@ -3,42 +3,47 @@ import java.util.Arrays;
 
 public class GameOfLifeNew {
 
+    public static int counter;
     public static int findChar(int[][] board, int i, int j){
         return board[i][j];
     }
 
-    public static int[][] playGame(int[][] board, int n){
-        System.out.println("Playing the game...");
-        int[][] newBoard = new int[n][n];
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                int count = 0;
-                for (int r = -1; r <=1; r++){
-                    for (int c = -1; c <= 1; c++){
-                        if ((i+r <0) ||
-                        (i+r > n-1) ||
-                        (j+c <0) ||
-                        (j+c > n-1)){
-                            continue;
+    public static void playGame(int[][] board, int n){
+        if (counter < 20) {
+            System.out.println("\nPlaying the game...");
+            int[][] newBoard = new int[n][n];
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    int count = 0;
+                    for (int r = -1; r <=1; r++){
+                        for (int c = -1; c <= 1; c++){
+                            if ((i+r <0) ||
+                                    (i+r > n-1) ||
+                                    (j+c <0) ||
+                                    (j+c > n-1)){
+                                continue;
+                            }
+                            count += board[i+r][j+c];
                         }
-                    count += board[i+r][j+c];
+                    }
+                    count -= board[i][j];
+                    if (board[i][j] == 1 && count < 2){
+                        newBoard[i][j] = 0;
+                    } else if (board[i][j] == 1 && (count == 2 || count == 3)){
+                        newBoard[i][j] = 1;
+                    } else if (board[i][j] == 1 && count > 3){
+                        newBoard[i][j] = 0;
+                    } else if (board[i][j] == 0 && count == 3){
+                        newBoard[i][j] = 1;
+                    } else {
+                        newBoard[i][j] = board[i][j];
                     }
                 }
-                count -= board[i][j];
-                if (board[i][j] == 1 && count < 2){
-                    newBoard[i][j] = 0;
-                } else if (board[i][j] == 1 && (count == 2 || count == 3)){
-                    newBoard[i][j] = 1;
-                } else if (board[i][j] == 1 && count > 3){
-                    newBoard[i][j] = 0;
-                } else if (board[i][j] == 0 && count == 3){
-                    newBoard[i][j] = 1;
-                } else {
-                    newBoard[i][j] = board[i][j];
-                }
             }
+            printBoard(newBoard, n);
+            counter++;
+            playGame(newBoard, n);
         }
-        return newBoard;
     }
 
     public static void printBoard(int[][] board, int n){
@@ -55,7 +60,7 @@ public class GameOfLifeNew {
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 10;
         int[][] board = new int[n][n];
 
         int min = 1;
@@ -68,16 +73,13 @@ public class GameOfLifeNew {
                 if (rand % 2 == 0){
                     board[i][j] = 1;
                     System.out.print("X ");
-                    //System.out.print(board[i][j]+" ");
                 } else {
                     board[i][j] = 0;
                     System.out.print("O ");
-                    //System.out.print(board[i][j]+" ");
                 }
             }
             System.out.println("");
         }
-        int[][] newBoard = playGame(board, n);
-        printBoard(newBoard, n);
+        playGame(board, n);
     }
 }
