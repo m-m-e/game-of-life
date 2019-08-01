15328853a41813ca56a3a72b8c8348949095c9ec
m-m-e <megmyfedwards@gmail.com>
2019-08-01 16:28:05 +0200

    Add comments to improve game

--- src/GameOfLifeNew.java
+++ src/GameOfLifeNew.java
@@ -1,18 +1,81 @@
 import java.lang.Math;
-import java.util.Arrays;
 
 public class GameOfLifeNew {
 
-    public static int counter;
-    public static int findChar(int[][] board, int i, int j){
-        return board[i][j];
+    public static void main(String[] args) throws InterruptedException {
+        int n = 5;
+        boolean[][] board = new boolean[n][n];
+
+        //Random rnd = new Random();
+        //rdn.nextBoolean();
+        //change n in i to board.length and in j board[i].length
+        //set number of times to print as an integer e.g. int defaultTimes = 500;
+        //new function called 'createNewBoard'
+        /*
+        while (defaultTimes-- > 0) {
+            board = playGame(board);
+            printBoard(board);s
+        }
+        */
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                double rand = Math.random();
+                if (rand <= 0.5){
+                    board[i][j] = true;
+                    System.out.print("X ");
+                } else {
+                    board[i][j] = false;
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("");
+        }
+        boolean keepPlaying = true;
+        for (int i = 0; i < 10 && keepPlaying; i++){
+            board = playGame(board, n);
+            printBoard(board, n, keepPlaying);
+            checkBoard(board, n, keepPlaying);
+            Thread.sleep(300);
+        }
+
+        // do...
+        //playgame, printboard etc.
+        // while (oldBoard == null || hasChanges(newBoard, oldBoard))
+        //need currentBoard and oldBoard = null boolean[][]s
+        /*
+            !Arrays.deepEquals(currentBoard, oldBoard);
+         */
     }
 
-    public static int[][] playGame(int[][] board, int n){
-        int[][] newBoard = new int[n][n];
+    public static boolean[][] playGame(boolean[][] board, int n){
+        boolean[][] newBoard = new boolean[n][n];
             System.out.println("\nPlaying the game...");
             for (int i = 0; i < n; i++){
                 for (int j = 0; j < n; j++){
+                    /*new function for counting the neighbours => count living neighbours
+                        let aliveNeighbours = 0;
+                        for (int rowDiff = -1; rowDiff <= 1; rowdiff++){
+                            for (int colDiff = -1; colDiff <= 1; coldiff++){
+                                int row = currentRow + rowDiff;
+                                int col = currentColumn + colDiff;
+
+                                if (canAccess(board, row, col)){
+                                    if (row != currentRow || col != currentColumn) OR this could be
+                                    boolean isCurrentCell = row == currentRow && col == currentColumn
+                                    {
+
+                                    if (board[row][col]){
+                                        aliveNeighbours++;
+                                    }
+                                }
+                            }
+                        }
+                    */
+
+                    /*
+                    New function canAccess => receives matrix, i, j;
+                        return i >=0 && j >=0 && i < matrix.length && j < matrix[i].length;
+                     */
                     int count = 0;
                     for (int r = -1; r <=1; r++){
                         for (int c = -1; c <= 1; c++){
@@ -22,35 +85,52 @@ public class GameOfLifeNew {
                                     (j+c > n-1)){
                                 continue;
                             }
-                            count += board[i+r][j+c];
+                            if (board[i+r][j+c] == true) {
+                                count++;
+                            }
                         }
                     }
-                    count -= board[i][j];
-                    if (board[i][j] == 1 && count < 2){
-                        newBoard[i][j] = 0;
-                    } else if (board[i][j] == 1 && (count == 2 || count == 3)){
-                        newBoard[i][j] = 1;
-                    } else if (board[i][j] == 1 && count > 3){
-                        newBoard[i][j] = 0;
-                    } else if (board[i][j] == 0 && count == 3){
-                        newBoard[i][j] = 1;
+
+                    /* instead of this logic...
+                    boolean currentCellIsAlive = currentBoard[i][j]
+                        if (currentCellIsAlive) {
+                            newBoard[i][j] = (livingNeighboursCount == 2 || livingNeighboursCount == 3);
+                        } else {
+                            newBoard[i][j] = (livingNeighboursCount == 3);
+                        }
+                     */
+                    if (board[i][j] == true) {
+                        count--;
+                    }
+                    if (board[i][j] == true && count < 2){
+                        newBoard[i][j] = false;
+                    } else if (board[i][j] == true && (count == 2 || count == 3)){
+                        newBoard[i][j] = true;
+                    } else if (board[i][j] == true && count > 3){
+                        newBoard[i][j] = false;
+                    } else if (board[i][j] == false && count == 3){
+                        newBoard[i][j] = true;
                     } else {
                         newBoard[i][j] = board[i][j];
                     }
                 }
             }
-            printBoard(newBoard, n);
         return newBoard;
     }
 
-    //booleans
-    //stops if no changes
     //stops if all dead
+    //stops if no changes
 
-    public static void printBoard(int[][] board, int n){
+    public static boolean printBoard(boolean[][] board, int n, boolean keepPlaying){
+        int aliveCount = 0;
+
+        //Can use ternary operators!
+        // System.out.print(board[i][j] ? 'X ' : '  ';
+
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                if (board[i][j] == 1){
+                if (board[i][j] == true){
+                    aliveCount++;
                     System.out.print("X ");
                 } else {
                     System.out.print("  ");
@@ -58,32 +138,29 @@ public class GameOfLifeNew {
             }
             System.out.println("");
         }
+        if (aliveCount == 0){
+            keepPlaying = false;
+        }
+        return keepPlaying;
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        int n = 10;
-        int[][] board = new int[n][n];
-
-        int min = 1;
-        int max = 100;
-        int range = max-min + 1;
-
+    public static boolean checkBoard(boolean[][] board, int n, boolean keepPlaying){
+        int changes = 0;
         for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                int rand = (int)(Math.random() * range) + min;
-                if (rand % 2 == 0){
-                    board[i][j] = 1;
-                    System.out.print("X ");
-                } else {
-                    board[i][j] = 0;
-                    System.out.print("  ");
+            for (int j = 0; j < n; j++) {
+                /*if (board[i][j] != otherBoard[i][j]){
+                return true;
+                }*/
+                if (j-1 >= 0 && board[j] != board[j-1]){
+                    changes++;
                 }
             }
-            System.out.println("");
         }
-        for (int i = 0; i < 500; i++){
-            board = playGame(board, n);
-            Thread.sleep(100);
+        System.out.println("changes: "+changes);
+        if (changes > 0){
+            return keepPlaying;
+        } else {
+            return !keepPlaying;
         }
     }
 }
